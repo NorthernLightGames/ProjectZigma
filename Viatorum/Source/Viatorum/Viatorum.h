3f710aacedb9707c5d28df710d7a3e492460e8d5
Righatherlue <gustavpeterberg@hotmail.se>
2016-04-10 18:05:36 +0200

    gfgdf
    gfgfd

--- Viatorum/Source/Viatorum/Viatorum.h
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "Engine.h"
-
-#define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White,text)
